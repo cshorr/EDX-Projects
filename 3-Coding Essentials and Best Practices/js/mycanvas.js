29e7f55a66ab97999de394dddd869360c1acf39a
chris shortt <cshortt707@gmail.com>
2025-06-18 13:15:59 -0700

    added text

--- 3-Coding Essentials and Best Practices/js/mycanvas.js
+++ 3-Coding Essentials and Best Practices/js/mycanvas.js
@@ -1,5 +1,5 @@
 var canvas, ctx;
-window.onload = init 
+window.onload = init;
 function init() {
   console.log("Canvas init running");
 
@@ -11,13 +11,19 @@ function init() {
   ctx.strokeStyle = "lightgreen";
   ctx.lineWidth = 5;
   ctx.strokeRect(50, 10, 100, 100);
+  ctx.fillStyle = "white"; // Text color
+  ctx.font = "12px Arial"; // Font and size
+  ctx.fillText("my green box ", 65, 65); // x and y (inside the box)
 
   ctx.fillStyle = "red";
-  ctx.fillRect(160, 10, 60, 60);
+  ctx.fillRect(160, 10, 70, 60);
   ctx.strokeStyle = "yellow";
   ctx.setLineDash([5, 5]); // dashed
   ctx.lineWidth = 2;
-  ctx.strokeRect(160, 10, 60, 60);
+  ctx.strokeRect(160, 10, 70, 60);
+  ctx.fillStyle = "white"; // Text color
+  ctx.font = "11px Arial"; // Font and size
+  ctx.fillText("My Red Box ", 165, 45); // x and y (inside the box)
 
   ctx.fillStyle = "grey";
   ctx.fillRect(160, 90, 140, 100);
@@ -25,6 +31,9 @@ function init() {
   ctx.setLineDash([10, 3, 3, 3]); // dash-dot pattern
   ctx.lineWidth = 8;
   ctx.strokeRect(160, 90, 140, 100);
+  ctx.fillStyle = "yellow"; // Text color 
+  ctx.font = "20px Arial"; // Font and size 
+  ctx.fillText("My Grey Box ", 170, 142); // x and y (inside the box)
 
   ctx.fillStyle = "purple";
   ctx.fillRect(235, 22, 120, 50);
@@ -32,9 +41,12 @@ function init() {
   ctx.setLineDash([3, 6]); // dotted
   ctx.lineWidth = 2;
   ctx.strokeRect(235, 20, 120, 50);
+  ctx.fillStyle = "white"; // Text color
+  ctx.font = "14px Arial"; // Font and size
+  ctx.fillText("My Purple Box ", 249, 45); // x and y (inside the box)
 
   drawTwoRectangles();
-};
+}
 
 function drawTwoRectangles() {
   console.log("Draw function works.");
